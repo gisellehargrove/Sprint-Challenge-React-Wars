3fec6729dd5e923866ea60785a444f8492f5f229
gisellehargrove <gisellehargrove@gmail.com>
2019-07-19 12:06:24 -0400

    created character component

---
+++ starwars/src/components/Character/Character.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+
+function Character(props) {
+  console.log(props.data)
+  return (
+    <Card>
+
+    </Card>
+  )
+};
+
+export default Character;
